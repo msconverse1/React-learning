3ba654353a7c9df62dbcae34b6fc04b0d49b5771
msconverse1 <msconverse@fullsail.edu>
2019-07-30 11:34:34 -0500

    Preparing Proj and Creating Comp
    Created Base project that renders
    "Welcome to Account-Owner Application"
    in Red text

---
+++ accountownerclient/src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Col,Row} from 'react-bootstrap';
+import './Home.css';
+
+const home = (props) => {
+    return (
+        <Row>
+            <Col md={12}>
+                <div className={'homeText'}>
+                    "Welcome To Account-Owner Application"
+                </div>
+            </Col>
+        </Row>
+    )
+}
+export default home;
--- accountownerclient/src/containers/App.js
+++ accountownerclient/src/containers/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Layout from '../components/Layout/Layout';
- 
+import Home from '../components/Home/Home';
+
 class App extends Component {
   render() {
     return (
       <Layout>
-        <strong>This content is going to be rendered as the props.children inside the Layout component.</strong>
+        <Home/>
       </Layout>
     );
   }
